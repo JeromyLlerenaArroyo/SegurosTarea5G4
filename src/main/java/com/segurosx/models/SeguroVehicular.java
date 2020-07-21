e1447db8fe833f81f5caf4bae792646a8ba22306
josehinojosaacevedo <66885102+josehinojosaacevedo@users.noreply.github.com>
2020-07-21 18:06:47 -0500

    pregunta 5
    pregunta 5

--- src/main/java/com/segurosx/models/SeguroVehicular.java
+++ src/main/java/com/segurosx/models/SeguroVehicular.java
@@ -1,8 +1,10 @@
 package com.segurosx.models;
 
 import com.segurosx.models.patterns.CoberturaBasicaVehicular;
-import com.segurosx.models.patterns.CoberturaPorChoqueDecorator;
-import com.segurosx.models.patterns.CoberturaTodoRiesgoDecorator;
+//import com.segurosx.models.patterns.CoberturaPorChoqueDecorator;
+//import com.segurosx.models.patterns.CoberturaTodoRiesgoDecorator;
+//import com.segurosx.models.patterns.CoberturaPorRoboDecorator;
+import com.segurosx.models.patterns.CoberturaDecoratorFactory;
 
 public class SeguroVehicular extends Seguro implements INivelRiesgo {
 
@@ -36,8 +38,10 @@ public class SeguroVehicular extends Seguro implements INivelRiesgo {
 
     public void calcularCobeturaVehicular() {
 
-        this.cobertura = new CoberturaTodoRiesgoDecorator(
-                            new CoberturaBasicaVehicular());
+        //this.cobertura = new CoberturaTodoRiesgoDecorator(
+        //                    new CoberturaBasicaVehicular());
+        CoberturaDecoratorFactory cdf = new CoberturaDecoratorFactory();
+        this.cobertura = cdf.createCoberturaDecorator(cdf.createCoberturaDecorator(cdf.createCoberturaDecorator(new CoberturaBasicaVehicular(),"todoriesgo"),"porrobo"),"porchoque");
         cobertura.calculaCobertura();        
     }
 }
